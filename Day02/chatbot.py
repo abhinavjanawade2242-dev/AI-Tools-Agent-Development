6eb599d73410fe96a586baf2ac770460c7fc74cb
Abhinav <abhinavjanawade2242@gmail.com>
2026-08-30 22:18:33 +0530

    Day02 of learning AI-tools

--- Day02/chatbot.py
+++ Day02/chatbot.py
@@ -1,18 +1,18 @@
-from openai import OpenAI
-from dotenv import load_dotenv
-import os
+from openai import OpenAI   #python to access openAI SDK
+from dotenv import load_dotenv  #lets python read variables from .env
+import os   #used to retrieve api key
 
-load_dotenv()
+load_dotenv()   #this will load .env
 
 client = OpenAI(
     api_key=os.getenv("OPENAI_API_KEY")
-)
+)   #this retrieves the value of API key
 
 try:
     response = client.responses.create(
         model="gpt-5.6-luna",
         input="Explain Python functions to a beginner."
-    )
+    )   #API request
 
     print(response.output_text)
 
